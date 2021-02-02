71e61771aceb25fd98bdf546d8e8b46e3a4ad10c
tanvir2021 <78404958+tanvir2021@users.noreply.github.com>
2021-02-02 21:13:27 +0600

    Web tech lab task 1

---
+++ Registration.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> My web page </title>
+</head>
+<body>
+<h1> Exercise </h1>
+<p> 1. Desiging a login form  </p>
+
+<h1> Registration </h1>
+
+<br><br>
+
+<form>
+<table>
+<tr>
+<td> Name   : </td>
+<td><input type="text" id="fname" name="fname"></td>
+</tr>
+
+<tr>
+<td> Name   : </td>
+<td><input type="text" id="fname" name="fname"></td>
+</tr>
+
+<tr>
+<td> Email  : </td>
+<td><input type="text" id="fname" name="fname"></td>
+</tr>
+
+<tr>
+<td> User Name   : </td>
+<td><input type="text" id="fname" name="fname"></td>
+</tr>
+
+<tr>
+<td> Password   : </td>
+<td><input type="text" id="fname" name="fname"></td>
+</tr>
+
+<tr>
+<td> Confirm Password  : </td>
+<td><input type="text" id="fname" name="fname"></td>
+</tr>
+<tr>
+<td><label1 for="Birthday">Birthday:<label1></td>
+<td><input type="date" id="Birthday" name="Birthday"></td>
+</tr>
+
+</table>
+<br><br>
+<p> Gender  </p>
+<tr>
+<td> Male </td>
+<input type="radio" id="Gender" name="Gender" value="Male"> 
+</tr>
+
+<tr>
+<td> Female</td>
+<input type="radio" id="Gender" name="Gender" value="Male"> 
+</tr>
+
+<tr>
+<td> Other </td>
+<input type="radio" id="Gender" name="Gender" value="Male"> 
+</tr>
+<br><br>
+<tr>
+<input type="Submit" Value= "Submit">
+<input type="Reset" Value= "Reset">
+</tr>
+</body>
+</html>
